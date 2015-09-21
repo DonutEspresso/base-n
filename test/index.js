2da841cec773840f3b4571a14410c49d0ddb1a83
Alex Liu <donutespresso@gmail.com>
2015-09-21 12:05:35 -0700

    add cli support. address PR comments

--- test/index.js
+++ test/index.js
@@ -1,166 +1,226 @@
 'use strict';
 
-// internal files
-var baseN = require('../lib');
 
 // external files
 var assert = require('chai').assert;
+var exec = require('child_process').exec;
+var path = require('path');
+
+// internal files
+var baseN = require('../lib');
+
 
 describe('baseN', function() {
 
-    it('should find correct fixed length', function() {
-        var b64;
-        // 64
-        // 4096
-        // 262144
-        // 16777216
-        b64 = baseN.create({
-            max: 63
-        });
-        assert.equal(b64.length, 1);
+    describe('Javascript API', function() {
 
-        b64 = baseN.create({
-            max: 64
-        });
-        assert.equal(b64.length, 2);
+        it('should find correct fixed length', function() {
+            var b64;
+            // 64
+            // 4096
+            // 262144
+            // 16777216
+            b64 = baseN.create({
+                max: 63
+            });
+            assert.equal(b64.length, 1);
 
-        b64 = baseN.create({
-            max: 4096
-        });
-        assert.equal(b64.length, 3);
+            b64 = baseN.create({
+                max: 64
+            });
+            assert.equal(b64.length, 2);
 
-        b64 = baseN.create({
-            max: 262144
+            b64 = baseN.create({
+                max: 4096
+            });
+            assert.equal(b64.length, 3);
+
+            b64 = baseN.create({
+                max: 262144
+            });
+            assert.equal(b64.length, 4);
         });
-        assert.equal(b64.length, 4);
+
+
+        it('should generate fixed length string', function() {
+            var x;
+            var b64;
+
+            b64 = baseN.create({
+                length: 5
+            });
+            x = b64.encode(5);
+            assert.isString(x);
+            assert.lengthOf(x, 5);
+
+            b64 = baseN.create({
+                length: 2
+            });
+            x = b64.encode(5);
+            assert.isString(x);
+            assert.lengthOf(x, 2);
+
+            // can also fix length by giving a max integer value allowed.
+            // this should be two characters. (64^2)
+            b64 = baseN.create({
+                max: 4095
+            });
+            x = b64.encode(5);
+            assert.isString(x);
+            assert.lengthOf(x, 2);
+        });
+
+
+        it('should symmetric encode/decode', function(done) {
+
+            this.timeout(10000);
+
+            var b64 = baseN.create({
+                max: 1000000
+            });
+            var accumulator = {};
+
+            for (var i = 0; i < 1000000; i++) {
+                var encoded = b64.encode(i);
+                var decoded = b64.decode(encoded);
+
+                // ensure symmetrical encoding/decoding
+                assert.equal(decoded, i);
+                // ensure this encoded value is unique and not seen yet
+                assert.notOk(accumulator[encoded]);
+                // add it to accumulator
+                accumulator[encoded] = true;
+            }
+
+            done();
+        });
+
+
+        it('should support base-n', function() {
+
+            // binary tests
+            var b2 = baseN.create({
+                base: 2
+            });
+
+            assert.equal(b2.encode(1), '1');
+            assert.equal(b2.encode(3), '11');
+            assert.equal(b2.encode(8), '1000');
+            assert.equal(b2.encode(9), '1001');
+        });
+
+
+        it('should support base-n with arbitrary characters', function() {
+
+            // binary tests
+            var b2 = baseN.create({
+                characters: '_-?',
+                base: 2
+            });
+
+            assert.equal(b2.encode(1), '-');
+            assert.equal(b2.encode(3), '--');
+            assert.equal(b2.encode(8), '-___');
+            assert.equal(b2.encode(9), '-__-');
+        });
     });
 
 
-    it('should generate fixed length string', function() {
-        var x;
-        var b64;
+    describe('CLI', function() {
 
-        b64 = baseN.create({
-            length: 5
-        });
-        x = b64.encode(5);
-        assert.isString(x);
-        assert.lengthOf(x, 5);
+        var cliPath = path.join(__dirname, '../bin/cli.js');
 
-        b64 = baseN.create({
-            length: 2
-        });
-        x = b64.encode(5);
-        assert.isString(x);
-        assert.lengthOf(x, 2);
-
-        // can also fix length by giving a max integer value allowed.
-        // this should be two characters. (64^2)
-        b64 = baseN.create({
-            max: 4095
-        });
-        x = b64.encode(5);
-        assert.isString(x);
-        assert.lengthOf(x, 2);
-    });
-
-
-    it('should throw when generating string larger than allowed length',
-    function() {
-        var b64;
-
-        b64 = baseN.create({
-            length: 2
-        });
-
-        // 2 characters is 64^2 = 4096 max (0 based)
-        assert.doesNotThrow(function() {
-            b64.encode(4095);
-        });
-        assert.throws(function() {
-            b64.encode(4096);
+        it('should encode', function(done) {
+            exec(cliPath + ' encode 11', function(err, stdout, stderr) {
+                assert.ifError(err);
+                // replace all new lines in output
+                var parsedOut = stdout.replace('\n', '');
+                assert.equal(parsedOut, 'b');
+                done();
+            });
         });
 
 
-        b64 = baseN.create({
-            max: 4095
-        });
-
-        assert.doesNotThrow(function() {
-            b64.encode(4095);
-        });
-        assert.throws(function() {
-            b64.encode(4096);
+        it('should decode', function(done) {
+            exec(cliPath + ' decode a', function(err, stdout, stderr) {
+                assert.ifError(err);
+                // replace all new lines in output
+                var parsedOut = stdout.replace('\n', '');
+                assert.equal(parsedOut, '10');
+                done();
+            });
         });
     });
 
 
-    it('should throw when encoding negative integers', function() {
-        var b64 = baseN.create();
+    describe('Error cases', function() {
 
-        assert.throws(function() {
-            b64.encode(-1);
+        it('should throw when generating string larger than allowed length',
+        function() {
+            var b64;
+
+            b64 = baseN.create({
+                length: 2
+            });
+
+            // 2 characters is 64^2 = 4096 max (0 based)
+            assert.doesNotThrow(function() {
+                b64.encode(4095);
+            });
+            assert.throws(function() {
+                b64.encode(4096);
+            });
+
+
+            b64 = baseN.create({
+                max: 4095
+            });
+
+            assert.doesNotThrow(function() {
+                b64.encode(4095);
+            });
+            assert.throws(function() {
+                b64.encode(4096);
+            }, RangeError);
+        });
+
+
+        it('should throw when encoding negative integers', function() {
+            var b64 = baseN.create();
+
+            assert.throws(function() {
+                b64.encode(-1);
+            }, RangeError);
+        });
+
+
+        it('should throw when encoding non integers', function() {
+            var b64 = baseN.create();
+
+            assert.throws(function() {
+                b64.encode('hello');
+            }, TypeError);
+        });
+
+
+        it('should throw when decoding unknown character', function() {
+            var b64 = baseN.create();
+
+            assert.throws(function() {
+                b64.decode('$');
+            });
+        });
+
+
+        it('should throw when value exceeds max supported Number value in JS',
+        function() {
+            var b64 = baseN.create();
+
+            assert.throws(function() {
+                b64.decode('-----------');
+            }, RangeError);
         });
     });
 
 
-    it('should throw when decoding unknown character', function() {
-        var b64 = baseN.create();
-
-        assert.throws(function() {
-            b64.decode('$');
-        });
-    });
-
-    it('should symmetric encode/decode', function(done) {
-
-        this.timeout(10000);
-
-        var b64 = baseN.create({
-            max: 1000000
-        });
-        var accumulator = {};
-
-        for (var i = 0; i < 1000000; i++) {
-            var encoded = b64.encode(i);
-            var decoded = b64.decode(encoded);
-
-            // ensure symmetrical encoding/decoding
-            assert.equal(decoded, i);
-            // ensure this encoded value is unique and not seen yet
-            assert.notOk(accumulator[encoded]);
-            // add it to accumulator
-            accumulator[encoded] = true;
-        }
-
-        done();
-    });
-
-
-    it('should support base-n', function() {
-
-        // binary tests
-        var b2 = baseN.create({
-            base: 2
-        });
-
-        assert.equal(b2.encode(1), '1');
-        assert.equal(b2.encode(3), '11');
-        assert.equal(b2.encode(8), '1000');
-        assert.equal(b2.encode(9), '1001');
-    });
-
-
-    it('should support base-n with arbitrary characters', function() {
-
-        // binary tests
-        var b2 = baseN.create({
-            characters: '_-'
-        });
-
-        assert.equal(b2.encode(1), '-');
-        assert.equal(b2.encode(3), '--');
-        assert.equal(b2.encode(8), '-___');
-        assert.equal(b2.encode(9), '-__-');
-    });
 });
