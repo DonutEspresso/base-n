1abf960ad7d312b81815878dccf738647cfb5d6a
Alex Liu <DonutEspresso@users.noreply.github.com>
2020-07-05 17:29:35 -0700

    breaking: support multi character encoding with new ctor API (#7)
    BREAKING CHANGE: `characters` option is now an array of string instead
    of a string.

--- bin/cli.js
+++ bin/cli.js
@@ -13,7 +13,7 @@ var options = [
     },
     {
         names: ['characters'],
-        type: 'string',
+        type: 'arrayOfString',
         help: 'Optional. Set of characters to encode with'
     },
     {
@@ -36,6 +36,7 @@ var parser = dashdash.createParser({
 
 function showHelp(opts) {
     var help = parser.help({includeEnv: true}).trimRight();
+    // eslint-disable-next-line no-console
     console.log('usage: basen [action] [value] [OPTIONS]\n\n'
             + 'action: encode | decode\n'
             + 'value: Number or string to encode/decode\n'
@@ -57,13 +58,16 @@ try {
 
     if (action === 'encode') {
         value = parseInt(value, 10);
+        // eslint-disable-next-line no-console
         console.log(instance.encode(value));
     } else if (action === 'decode') {
+        // eslint-disable-next-line no-console
         console.log(instance.decode(value));
     } else {
         showHelp(opts);
     }
 } catch (e) {
+    // eslint-disable-next-line no-console
     console.log(e.stack);
     process.exit(1);
 }
--- tools/changelog.js
+++ tools/changelog.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-/* eslint-disable no-console, no-process-exit */
+/* eslint-disable */
 
 'use strict';
 
