9b573eee608b2d5221d21c4371a4ec704162b09a
Alex Liu <DonutEspresso@users.noreply.github.com>
2020-07-05 17:52:09 -0700

    breaking: migrate to latest ES6 syntax. Support 10.x+ only. (#8)
    BREAKING CHANGE. Brings repo up to latest ES6 syntax. Add engines field
    for 10.x+. General repo maintenace and dev deps udpates.

--- bin/cli.js
+++ bin/cli.js
@@ -2,59 +2,59 @@
 
 'use strict';
 
-var dashdash = require('dashdash');
-var baseN = require('../lib');
+const dashdash = require('dashdash');
+const baseN = require('../lib');
 
-var options = [
+const options = [
     {
         names: ['help', 'h'],
         type: 'bool',
-        help: 'Print this help and exit.'
+        help: 'Print this help and exit.',
     },
     {
         names: ['characters'],
         type: 'arrayOfString',
-        help: 'Optional. Set of characters to encode with'
+        help: 'Optional. Set of characters to encode with',
     },
     {
         names: ['base'],
         type: 'number',
-        help: 'Optional. The base to encode/decode with'
+        help: 'Optional. The base to encode/decode with',
     },
     {
         names: ['length'],
         type: 'number',
-        help: 'Optional. maximum length of encoded ouptut.'
-    }
+        help: 'Optional. maximum length of encoded ouptut.',
+    },
 ];
 
-
-var parser = dashdash.createParser({
+const parser = dashdash.createParser({
     allowUnknown: true,
-    options: options
+    options: options,
 });
 
 function showHelp(opts) {
-    var help = parser.help({includeEnv: true}).trimRight();
+    const help = parser.help({ includeEnv: true }).trimRight();
     // eslint-disable-next-line no-console
-    console.log('usage: basen [action] [value] [OPTIONS]\n\n'
-            + 'action: encode | decode\n'
-            + 'value: Number or string to encode/decode\n'
-            + 'options:\n'
-            + help);
+    console.log(
+        'usage: basen [action] [value] [OPTIONS]\n\n' +
+            'action: encode | decode\n' +
+            'value: Number or string to encode/decode\n' +
+            'options:\n' +
+            help
+    );
 }
 
-
 try {
-    var opts = parser.parse(process.argv);
-    var action = opts._args[0];
-    var value = opts._args[1];
+    const opts = parser.parse(process.argv);
+    const action = opts._args[0];
+    let value = opts._args[1];
 
     if (opts.help) {
         showHelp(opts);
     }
 
-    var instance = baseN.create(opts);
+    const instance = baseN.create(opts);
 
     if (action === 'encode') {
         value = parseInt(value, 10);
@@ -69,7 +69,6 @@ try {
 } catch (e) {
     // eslint-disable-next-line no-console
     console.log(e.stack);
+    // eslint-disable-next-line no-process-exit
     process.exit(1);
 }
-
-
